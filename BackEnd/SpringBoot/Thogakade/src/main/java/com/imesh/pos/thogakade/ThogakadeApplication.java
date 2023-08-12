227af5afdaeccc0234bd6a6c710c0d04c1195be6
Imesh <imeshisuranga00@gmail.com>
2023-08-12 23:15:07 +0530

    Backend SpringBoot backend part started.This is initial commit

---
+++ BackEnd/SpringBoot/Thogakade/src/main/java/com/imesh/pos/thogakade/ThogakadeApplication.java
@@ -0,0 +1,13 @@
+package com.imesh.pos.thogakade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThogakadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThogakadeApplication.class, args);
+    }
+
+}
---
+++ BackEnd/SpringBoot/Thogakade/src/test/java/com/imesh/pos/thogakade/ThogakadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imesh.pos.thogakade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThogakadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
