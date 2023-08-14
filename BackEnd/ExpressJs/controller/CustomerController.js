328ac2d113dca55da8a36724c219abbb3d9e6432
Cat998877 <bitwhizmail1@gmail.com>
2023-08-14 13:15:42 +0530

    reated index.js,CustomerController and .env files.

--- BackEnd/ExpressJs/controller/CustomerController.js
+++ BackEnd/ExpressJs/controller/CustomerController.js
@@ -1,5 +1,17 @@
-const saveCustomer=(req,res)=>{
+const Customer = require('../model/CustomerSchema')
 
+const saveCustomer=(req,res)=>{
+    const customerDTO = new Customer({
+        id:req.body.id,
+        name:req.body.name,
+        address:req.body.address,
+        salary:req.body.salary
+    });
+    customerDTO.save().then(result=>{
+        response.staus(201).json(result);
+    }).catch(error=>{
+        response.staus(500).json(error);
+    });
 }
 
 const updateCustomer=(req,res)=>{
