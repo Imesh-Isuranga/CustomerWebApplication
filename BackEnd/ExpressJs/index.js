a91118dd1fc0a8a3da4cd22678558b500bd3edfa
Imesh-Isuranga <imeshisuranga00@gmail.com>
2023-08-14 17:36:27 +0530

    Folder Updated frontend

--- BackEnd/ExpressJs/index.js
+++ BackEnd/ExpressJs/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -9,6 +10,8 @@ const port = process.env.PORT;
 const CustomerRoute = require('./route/CustomerRoute');
 
 const app = express();
+
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
